f35f8ee120e35f86d015157cfec48c2ae54bd231
Juan Brito <juan.a.brito90@gmail.com>
2021-04-18 17:47:54 -0300

    Tema 4: Practica Layouts

--- android/app/src/main/java/ar/edu/utn/frba/mobile/clases/MainComposeActivity.kt
+++ android/app/src/main/java/ar/edu/utn/frba/mobile/clases/MainComposeActivity.kt
@@ -12,6 +12,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import ar.edu.utn.frba.mobile.clases.ui.main.HomeScreen
+import ar.edu.utn.frba.mobile.clases.ui.status_update.StatusUpdateScreen
 import ar.edu.utn.frba.mobile.clases.ui.theme.ClasesTheme
 
 class MainComposeActivity : ComponentActivity() {
@@ -27,6 +28,14 @@ class MainComposeActivity : ComponentActivity() {
 private fun App() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "home") {
-        composable("home") { HomeScreen() }
+        composable("home") {
+            HomeScreen(
+                onButtonClick = { navController.navigate("post") },
+                navController = navController)
+        }
+        composable("post") {
+            StatusUpdateScreen(
+                navController = navController)
+        }
     }
 }
--- android/app/src/main/java/ar/edu/utn/frba/mobile/clases/ui/main/MainViewModelResources.kt
+++ android/app/src/main/java/ar/edu/utn/frba/mobile/clases/ui/main/MainViewModelResources.kt
@@ -3,7 +3,8 @@ package ar.edu.utn.frba.mobile.clases.ui.main
 import android.content.Context
 import ar.edu.utn.frba.mobile.clases.R
 
-class MainViewModelResources(private val context: Context): MainViewModel.ResourceProvider {
+class MainViewModelResources(val context: Context): MainViewModel.ResourceProvider {
+
     override val salute: String
         get() = context.getString(R.string.salute)
 }
