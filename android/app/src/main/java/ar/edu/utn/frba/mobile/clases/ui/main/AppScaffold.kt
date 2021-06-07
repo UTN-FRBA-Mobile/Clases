ca011abdf32190fc837369701839889f1cb4e08b
Emanuel <emanuel@aijunasys.com.ar>
2021-06-06 21:47:31 -0300

    Ejemplo de WebView.

--- android/app/src/main/java/ar/edu/utn/frba/mobile/clases/ui/main/AppScaffold.kt
+++ android/app/src/main/java/ar/edu/utn/frba/mobile/clases/ui/main/AppScaffold.kt
@@ -1,6 +1,7 @@
 package ar.edu.utn.frba.mobile.clases.ui.main
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -16,6 +17,7 @@ import ar.edu.utn.frba.mobile.clases.ui.theme.ClasesTheme
 fun AppScaffold(
     title: String? = null,
     navController: NavController? = null,
+    actions: @Composable RowScope.() -> Unit = {},
     content: @Composable (PaddingValues) -> Unit) {
     val navigationIcon: (@Composable () -> Unit)? =
         if (navController?.previousBackStackEntry != null) {
@@ -35,6 +37,7 @@ fun AppScaffold(
                         Text(text = title ?: stringResource(id = R.string.app_name))
                     },
                     navigationIcon = navigationIcon,
+                    actions = actions,
                 )
             },
             content = {
