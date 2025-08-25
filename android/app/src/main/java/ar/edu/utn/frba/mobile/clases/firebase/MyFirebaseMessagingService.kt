rociochipian/ClasesRo

Demo Notificaciones

android/app/src/main/java/ar/edu/utn/frba/mobile/clases/firebase/MyFirebaseMessagingService.kt
@@ -1,0 +1,72 @@
+package ar.edu.utn.frba.mobile.clases.firebase
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import ar.edu.utn.frba.mobile.clases.MainActivity
+import ar.edu.utn.frba.mobile.clases.MyPreferences
+import ar.edu.utn.frba.mobile.clases.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.data.isNotEmpty()) {
+            //Todo
+        }
+
+        if (remoteMessage.notification != null) {
+            showNotification(remoteMessage.notification!!)
+        }
+    }
+
+    override fun onNewToken(newToken: String) {
+        MyPreferences.setFirebaseToken(this, newToken)
+
+        super.onNewToken(newToken)
+    }
+
+    private fun showNotification(notification: RemoteMessage.Notification) {
+        val intent = Intent(this, MainActivity::class.java)
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val requestCode = 0
+
+        val pendingIntent = PendingIntent.getActivity(this, requestCode, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(notification.title)
+            .setContentText(notification.body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notificationId = 0
+        notificationManager.notify(notificationId, notificationBuilder.build())
+    }
+
+    companion object {
+
+        private val CHANNEL_ID = "CHANNEL_1"
+    }
+}

UTN-FRBA-Mobile/Clases

android/app/src/main/java/ar/edu/utn/frba/mobile/clases/firebase/MyFirebaseMessagingService.kt
@@ -1,0 +1,72 @@
+package ar.edu.utn.frba.mobile.clases.firebase
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import ar.edu.utn.frba.mobile.clases.MainViewActivity
+import ar.edu.utn.frba.mobile.clases.MyPreferences
+import ar.edu.utn.frba.mobile.clases.R
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        if (remoteMessage.data.isNotEmpty()) {
+            //Todo
+        }
+
+        if (remoteMessage.notification != null) {
+            showNotification(remoteMessage.notification!!)
+        }
+    }
+
+    override fun onNewToken(newToken: String) {
+        MyPreferences.setFirebaseToken(this, newToken)
+
+        super.onNewToken(newToken)
+    }
+
+    private fun showNotification(notification: RemoteMessage.Notification) {
+        val intent = Intent(this, MainViewActivity::class.java)
+
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        val requestCode = 0
+
+        val pendingIntent = PendingIntent.getActivity(this, requestCode, intent,
+            PendingIntent.FLAG_ONE_SHOT)
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setContentTitle(notification.title)
+            .setContentText(notification.body)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notificationId = 0
+        notificationManager.notify(notificationId, notificationBuilder.build())
+    }
+
+    companion object {
+
+        private val CHANNEL_ID = "CHANNEL_1"
+    }
+}